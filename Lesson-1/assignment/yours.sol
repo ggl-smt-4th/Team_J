e640b1c45c79b1cf03d82f77676b83540e6c7cf3
blockfsd <40328254+blockfsd@users.noreply.github.com>
2018-06-18 23:02:40 +0800

    完成第一课作业

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,86 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint constant payDuration = 10 seconds;
+
+    address owner;
+    uint salary;
+    address employee;
+    uint lastPayday;
+
+    function Payroll() {
+        owner = msg.sender;
+        salary = 1 ether;
+        lastPayday = now;
+        employee = 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c;
+    }
+
+    /*
+    function updateEmployee(address e, uint s) {
+        require(msg.sender == owner);
+
+        if (employee != 0x0) {
+            uint payment = salary * (now - lastPayday) / payDuration;
+            employee.transfer(payment);
+        }
+
+        employee = e;
+        salary = s * 1 ether;
+        lastPayday = now;
+    }
+    */
+
+    //清算工资
+    function payoff() returns (bool) {
+        if (employee != 0x0) {
+            uint payment = salary * (now - lastPayday) / payDuration;
+            if (payment > this.balance) {
+                return false;
+            }
+            employee.transfer(payment);
+        }
+
+        lastPayday = now;
+        return true;
+    }
+
+    //更改员工地址
+    function updateEmployee(address e) {
+        require(msg.sender == owner);
+        if (payoff()) {
+            employee = e;
+        }
+    }
+
+    //更改员工薪水
+    function updateSalary(uint s) {
+        require(msg.sender == owner);
+        if (payoff()) {
+            salary = s * 1 ether;
+        }
+    }
+
+
+
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+
+    function getPaid() {
+        require(msg.sender == employee);
+
+        uint nextPayday = lastPayday + payDuration;
+        assert(nextPayday < now);
+
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+}
